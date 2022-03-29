15d0f285d1876e1188bbf94d4ce38b6d599606ab
229872 <bartek.dygasinski00@gmail.com>
2022-03-29 16:23:53 +0200

    Program

---
+++ Projekt/Projekt/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace Projekt
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("2 * 2 = " + Multiply(2,2));
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Projekt/Test2/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
